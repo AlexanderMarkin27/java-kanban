887fa2871cff04094c1433c44f24767758df5d73
Alexander Markin <alexander.markin@creamteam.de>
2023-12-13 22:40:46 +0100

    func: history to string

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import com.yandex.taskTracker.model.Epic;
 import com.yandex.taskTracker.model.SubTask;
 import com.yandex.taskTracker.model.Task;
 import com.yandex.taskTracker.service.FileBackedTasksManager;
+import com.yandex.taskTracker.service.HistoryManager;
 import com.yandex.taskTracker.service.TaskManager;
 import com.yandex.taskTracker.utils.Managers;
 
@@ -18,6 +19,7 @@ public class Main {
     public static void main(String[] args) {
        File file = new File("./resources/tasks.csv");
        FileBackedTasksManager manager = Managers.getFileBackedTasksManager(file);
+        HistoryManager manager1 = Managers.getDefaultHistory();
 
         Task task1 = new Task("Task 1", "this is task 1", Status.NEW);
         Task task2 = new Task("task 2", "this is task 2", Status.IN_PROGRESS);
@@ -28,6 +30,7 @@ public class Main {
         manager.getTaskByIndex(taskId1);
         manager.getTaskByIndex(taskId2);
         int taskIdUpdataed = manager.updateTask(task2);
+
     }
 
 
--- src/com/yandex/taskTracker/service/FileBackedTasksManager.java
+++ src/com/yandex/taskTracker/service/FileBackedTasksManager.java
@@ -1,15 +1,18 @@
 package com.yandex.taskTracker.service;
 
 import com.yandex.taskTracker.enums.Status;
+import com.yandex.taskTracker.enums.Type;
 import com.yandex.taskTracker.model.Epic;
 import com.yandex.taskTracker.model.SubTask;
 import com.yandex.taskTracker.model.Task;
+import com.yandex.taskTracker.utils.Managers;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class FileBackedTasksManager extends InMemoryTaskManager {
 
@@ -98,6 +101,40 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
     }
 
+    private Task fromString(String value) {
+        String[] taskData = value.split(",");
+        try {
+            switch (Type.valueOf(taskData[1])) {
+                case TASK:
+                    Task task = new Task(taskData[2], taskData[4], Status.valueOf(taskData[3]));
+                    task.setId(Integer.parseInt(taskData[0]));
+                    return task;
+                case EPIC:
+                    Epic epic = new Epic(taskData[2], taskData[4]);
+                    epic.setId(Integer.parseInt(taskData[0]));
+                    epic.setStatus(Status.valueOf(taskData[3]));
+                    return epic;
+                case SUBTASK:
+                    SubTask subTask = new SubTask(taskData[2], taskData[4], Status.valueOf(taskData[3])
+                            , Integer.parseInt(taskData[5]));
+                    subTask.setId(Integer.parseInt(taskData[0]));
+                    subTask.setStatus(Status.valueOf(taskData[3]));
+                    return subTask;
+            }
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так!!!");
+        }
+        return null;
+    }
+
+    public static String historyToString(HistoryManager manager) {
+        String[] arrayOfIds = new String[manager.getHistory().size()];
+        for(int i = 0; i <= arrayOfIds.length; i++) {
+            arrayOfIds[i] = String.valueOf(manager.getHistory().get(i).getId());
+        }
+        return String.join(",", arrayOfIds);
+    }
+
     private void save() {
         try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.file, StandardCharsets.UTF_8))){
             String fileHeader = "id,type,name,status,description,epic";
@@ -112,11 +149,14 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
                 fileWriter.write(epic.toString() + "\n");
             }
             fileWriter.write("\n");
+
+//            should be changed
+
             for (Task task: super.getHistory()) {
                 fileWriter.write(task.getId() + ",");
             }
         } catch (IOException e) {
-            System.out.println("Пиздец");
+            System.out.println("Что-то пошло не так!!!");
         }
     }
 }
